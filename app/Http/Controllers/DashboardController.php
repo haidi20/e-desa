cd11e8f710ec20f6d893304091571a64b9a16f39
haidi20 <nhaidii@yahoo.com>
2018-01-11 11:56:47 +0800

    'update'

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     public function index(){
-      return view('dashboard.index')
+      return view('dashboard.index');
     }
 }
