0092cd166aac9d7ebc6964caf482a55ec4cac389
haidi20 <nhaidii@yahoo.com>
2018-02-12 11:48:13 +0800

    'update'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        require_once app_path('App/Supports/Helper.php');
+        require_once app_path('Supports/Helpers.php');
     }
 }
